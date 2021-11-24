e912e6552911d874c0e240893a6b95f89bbf5b40
StephenOnGit <89786165+Stephen-1973@users.noreply.github.com>
2021-11-24 17:13:15 +0530

    Update Header.js

--- sections/Header.js
+++ sections/Header.js
@@ -4,7 +4,7 @@ import { motion, AnimatePresence } from 'framer-motion'
 import { useColorModeValue } from '@chakra-ui/react';
 
 function Header() {
-  const buttonBg = useColorModeValue("linear-gradient(120deg, transparent 0%, #fff 50%, teal 50%)", 'linear-gradient(120deg, transparent 0%, black 50%, teal 50%)')
+  const buttonBg = useColorModeValue("linear-gradient(120deg, white 0%, white 50%, teal 50%)", 'linear-gradient(120deg, transparent 0%,#000 50%, teal 50%)')
   
   return (
     <SimpleGrid columns={[1,1,2]} rows={[2,2,1]} flexDirection='row' h={['70vh','65vh','50vh']} w='100%' mx='auto' mb={0} mt={10}>
@@ -22,8 +22,8 @@ function Header() {
                 <Text>I know it&#39;s a <span style={{ fontWeight: 'bold' }}>flex to show off my desk setup</span> as the first thing on my website 😅. But it&#39;s the <span style={{ fontWeight: 'bold' }}>best thing I have in my life</span> and also <span style={{ fontWeight: 'bold' }}>I don&#39;t have any other ideas</span> to make my site better <span style={{ fontWeight: 'bold' }}>than displaying my absolute fortune desk setup</span></Text>
               </motion.div>
               <motion.div animate={{transform: 'translateY(3rem)'}} transition={{duration: 0.4}} initial={{transform: 'translateY(0rem)'}} exit={{transform: 'translateY(0rem)'}}>
-                <Button variant='outline' borderBottom='.2rem solid teal' transition='all .2s' _hover={{backgroundPosition: "100%",color:'white'}} backgroundSize='230%' bgImage={buttonBg}>
-                  Know more &rarr;
+                <Button role='group' variant='outline' borderBottom='.2rem solid teal' transition='all .2s' _hover={{backgroundPosition: "100%",color:'white'}} backgroundSize='230%' bgImage={buttonBg}>
+                  Know more <Text transition='all .2s ease-in' ml='.3rem' _groupHover={{marginLeft:".5rem"}}>&rarr;</Text>
                 </Button>
               </motion.div>
             </VStack>
@@ -34,3 +34,4 @@ function Header() {
 }
 
 export default Header
+
