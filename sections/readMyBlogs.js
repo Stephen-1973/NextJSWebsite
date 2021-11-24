6be5f427b4271d1d42f2c87589d71baecf5e656f
StephenOnGit <89786165+Stephen-1973@users.noreply.github.com>
2021-11-24 18:10:48 +0530

    Update readMyBlogs.js

--- sections/readMyBlogs.js
+++ sections/readMyBlogs.js
@@ -3,11 +3,11 @@ import { Box, Button, HStack,Text } from '@chakra-ui/react';
 function ReadMyBlogs() {
   return (
     <Box bgImage='url(/blog.avif),url(/blog.jpeg)' bgSize='cover' bgPos='center' w='80%' display='flex' justifyContent='center' py={10} mx='auto' backgroundColor='gray.200'>
-      <Button role='group' transition='all .2s' _hover={{backgroundPosition: '100%'}} backgroundImage='linear-gradient(240deg,transparent 0%,#fff 50%, salmon 50%)' backgroundSize='230%' backgroundPosition='0%'>
+      <Button role='group' transition='all .2s' _hover={{backgroundPosition: '100%',color:'gray.800'}} backgroundImage='linear-gradient(240deg, white 0%,#fff 50%, salmon 50%)' backgroundSize='230%' backgroundPosition='0%'>
         <Text>
           <HStack>
             <Text>Read My Blogs</Text>
-            <Text transition='all .2s' _groupHover={{marginLeft: '1rem'}}>&rarr;</Text>
+            <Text transition='all .2s ease-in' _groupHover={{marginLeft: '1rem'}}>&rarr;</Text>
           </HStack>
         </Text>
       </Button>
@@ -16,3 +16,4 @@ function ReadMyBlogs() {
 }
 
 export default ReadMyBlogs
+
