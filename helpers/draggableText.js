7a6e43817719da695a6e365380d2b59d0c3e1d0f
StephenOnGit <89786165+Stephen-1973@users.noreply.github.com>
2021-11-24 17:02:29 +0530

    Update draggableText.js

--- helpers/draggableText.js
+++ helpers/draggableText.js
@@ -4,11 +4,12 @@ import { useColorModeValue } from '@chakra-ui/react';
 
 
 function DraggableText() {
-  const bgDraggableText = useColorModeValue('gray.800','gray.100')
+  const bgDraggableText = useColorModeValue('gray.100','gray.800')
+  const draggableText = useColorModeValue("gray.800",'gray.100')
   return (
     <>
     <motion.div drag style={{ position: 'absolute' }}>
-        <Text display={['none','none','inline-block']} fontSize='4rem' fontWeight='regular' textTransform='uppercase' color={bgDraggableText} bg={bgDraggableText}>My WorkSpace</Text>
+        <Text display={['none','none','inline-block']} fontSize='4rem' fontWeight='regular' textTransform='uppercase' color={draggableText} bg={bgDraggableText}>My WorkSpace</Text>
       </motion.div>
       <motion.svg style={{ position: 'absolute', transform: 'translate(7rem,5.5rem)' }} initial={{opacity:1}} animate={{opacity: 0}} transition={{duration: 4}} xmlns="http://www.w3.org/2000/svg" width="160" height="91.479" viewBox="0 0 187.111 91.479">
         <g id="Group_4" data-name="Group 4" transform="translate(-719.889 -687.521)">
