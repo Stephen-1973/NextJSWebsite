6fd722ecfee1b2fa48ec55b557491fb40eae570d
StephenOnGit <89786165+Stephen-1973@users.noreply.github.com>
2021-11-24 18:02:43 +0530

    Update NavBar.js

--- sections/NavBar.js
+++ sections/NavBar.js
@@ -1,20 +1,40 @@
 import ThemeToggleButton from '../helpers/theme-toggle-button';
-import { HStack, Box, IconButton,Heading, Text, Flex, Image } from '@chakra-ui/react';
+import {
+  HStack, Box, IconButton,Heading, Flex, Icon, Image, MenuList,Menu,MenuButton,MenuItem,Link, Text} from '@chakra-ui/react';
 import { AnimatePresence,motion } from 'framer-motion';
 import { useColorModeValue } from '@chakra-ui/react';
-import { HamburgerIcon} from '@chakra-ui/icons'
+import { HamburgerIcon } from '@chakra-ui/icons'
+
+import NextLink from 'next/link'
+
 
 
 function NavBar() {
-  const bgSvg = useColorModeValue('black', '#fff')
-  const HamburgerIconBox = useColorModeValue('black','white')
+  const LinkItem = ({ href, path, _target, children, ...props }) => {
+  const active = path === href
+  const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
   return (
-    <AnimatePresence>
-      <motion.div initial={{y:-20,opacity:0}} animate={{y:0,opacity: 1}} transition={{duration: 0.2}}>
-        <Box w='full' alignItems='center' display='flex' justifyContent={['space-evenly','space-evenly']}>
-          <Flex w='50%' justifyContent='space-evenly' alignItems='center'>
-            <HStack alignItems='center' justifyContent='space-evenly' spacing={2}>
-              <motion.svg style={{ transform:'rotate(-3deg)' }} xmlns="http://www.w3.org/2000/svg" width="2rem" height="3rem" viewBox="0 0 229.211 132.538">
+    <NextLink href={href} passHref>
+      <Link
+        p={2}
+        bg={active ? 'grassTeal' : undefined}
+        color={active ? '#202023' : inactiveColor}
+        _target={_target}
+        {...props}
+      >
+        {children}
+      </Link>
+    </NextLink>
+  )
+}
+
+  const bgSvg = useColorModeValue('black', '#fff')
+
+
+
+  const GithubLogo = () => (<svg stroke="currentColor" fill="black" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M256 32C132.3 32 32 134.9 32 261.7c0 101.5 64.2 187.5 153.2 217.9a17.56 17.56 0 003.8.4c8.3 0 11.5-6.1 11.5-11.4 0-5.5-.2-19.9-.3-39.1a102.4 102.4 0 01-22.6 2.7c-43.1 0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1 1.4-14.1h.1c22.5 2 34.3 23.8 34.3 23.8 11.2 19.6 26.2 25.1 39.6 25.1a63 63 0 0025.6-6c2-14.8 7.8-24.9 14.2-30.7-49.7-5.8-102-25.5-102-113.5 0-25.1 8.7-45.6 23-61.6-2.3-5.8-10-29.2 2.2-60.8a18.64 18.64 0 015-.5c8.1 0 26.4 3.1 56.6 24.1a208.21 208.21 0 01112.2 0c30.2-21 48.5-24.1 56.6-24.1a18.64 18.64 0 015 .5c12.2 31.6 4.5 55 2.2 60.8 14.3 16.1 23 36.6 23 61.6 0 88.2-52.4 107.6-102.3 113.3 8 7.1 15.2 21.1 15.2 42.5 0 30.7-.3 55.5-.3 63 0 5.4 3.1 11.5 11.4 11.5a19.35 19.35 0 004-.4C415.9 449.2 480 363.1 480 261.7 480 134.9 379.7 32 256 32z"></path></svg>)
+  const Logo = () => (
+    <motion.svg style={{ transform:'rotate(-3deg)' }} xmlns="http://www.w3.org/2000/svg" width="2rem" height="3rem" viewBox="0 0 229.211 132.538">
                 <g id="Group_5" data-name="Group 5" transform="translate(-611.694 -686.263)">
                   <path id="Path_1" data-name="Path 1" d="M623.075,731.979c.3-45.76,46.016-43.234,46.016-43.234H785.9l-86.277,43.234Z" transform="translate(55 -2.458)" fill={bgSvg}/>
                   <path id="Path_2" data-name="Path 2" d="M622.6,727.521c.3-41.045,46.914-38.779,46.914-38.779H787.408l-88.375,38.779Z" transform="translate(1399.102 1507.521) rotate(-180)" fill={bgSvg}/>
@@ -26,26 +46,69 @@ function NavBar() {
                   <path id="Polygon_3" data-name="Polygon 3" d="M18,0,36,16H0Z" transform="translate(741 764)" fill={bgSvg}/>
                 </g>
               </motion.svg>
-              <Heading variant='page-title'>
-                Stephen 
-              </Heading>
-            </HStack>
-            <Text display={['none','none','auto']} fontSize='110%'>
-              Work
-            </Text>
-            <Box display={['none','none','auto']} cursor='pointer' fontSize='110%'>
-              <HStack>
-                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M256 32C132.3 32 32 134.9 32 261.7c0 101.5 64.2 187.5 153.2 217.9a17.56 17.56 0 003.8.4c8.3 0 11.5-6.1 11.5-11.4 0-5.5-.2-19.9-.3-39.1a102.4 102.4 0 01-22.6 2.7c-43.1 0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1 1.4-14.1h.1c22.5 2 34.3 23.8 34.3 23.8 11.2 19.6 26.2 25.1 39.6 25.1a63 63 0 0025.6-6c2-14.8 7.8-24.9 14.2-30.7-49.7-5.8-102-25.5-102-113.5 0-25.1 8.7-45.6 23-61.6-2.3-5.8-10-29.2 2.2-60.8a18.64 18.64 0 015-.5c8.1 0 26.4 3.1 56.6 24.1a208.21 208.21 0 01112.2 0c30.2-21 48.5-24.1 56.6-24.1a18.64 18.64 0 015 .5c12.2 31.6 4.5 55 2.2 60.8 14.3 16.1 23 36.6 23 61.6 0 88.2-52.4 107.6-102.3 113.3 8 7.1 15.2 21.1 15.2 42.5 0 30.7-.3 55.5-.3 63 0 5.4 3.1 11.5 11.4 11.5a19.35 19.35 0 004-.4C415.9 449.2 480 363.1 480 261.7 480 134.9 379.7 32 256 32z"></path></svg>
-                <Text>Source</Text>
-              </HStack>
-            </Box>
-            <Text display={['none','none','auto']} fontSize='110%'>
-              Blogs
-            </Text>
+  )
+
+  
+  return (
+    <AnimatePresence>
+      <motion.div initial={{ y: -20, opacity: 0 }} animate={{ y: 0, opacity: 1 }} exit={{y:-20,opacity:0}} transition={{ duration: 1 }}>
+        <HStack justifyContent='space-between' w='80%' m='auto'>
+          <Flex alignItems='center' flexDirection='row'>
+            <IconButton bg='transparent' _hover={{bg: 'transparent'}}>
+              <Icon as={Logo}/>
+            </IconButton>
+            <Heading variant='page-title'>Stephen</Heading>
+            <HStack ml='2rem' spacing={5} display={{ base: 'none', md: 'flex' }}>
+              <NextLink role='group' href='/work'>
+                <Link>About</Link>
+              </NextLink>
+              <NextLink href='/work'>
+                <Link>Work</Link>
+              </NextLink>
+              <NextLink href='/work'>
+                <Link>Blogs</Link>
+              </NextLink>
+              <NextLink href='/work'>
+                <HStack>
+                  <GithubLogo />
+                  <Link>
+                    View Source
+                  </Link>
+                </HStack>
+              </NextLink>
+          </HStack>
           </Flex>
-          <ThemeToggleButton />
-          <IconButton icon={<HamburgerIcon/>}></IconButton>
-        </Box>
+          <HStack spacing={5}>
+            <ThemeToggleButton/>
+            <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+            <Menu isLazy id="navbar-menu">
+                <MenuButton
+                  as={IconButton}
+                  icon={<HamburgerIcon />}
+                  variant="outline"
+                  aria-label="Options"
+                />
+                <MenuList>
+                  <NextLink href="/" passHref>
+                    <MenuItem as={Link}>About</MenuItem>
+                  </NextLink>
+                  <NextLink href="/works" passHref>
+                    <MenuItem as={Link}>Works</MenuItem>
+                  </NextLink>
+                  <NextLink href="/posts" passHref>
+                    <MenuItem as={Link}>Blogs</MenuItem>
+                  </NextLink>
+                  <MenuItem
+                    as={Link}
+                    href="https://github.com/craftzdog/craftzdog-homepage"
+                  >
+                    View Source
+                    </MenuItem>
+                </MenuList>
+              </Menu>
+            </Box>
+          </HStack>
+        </HStack>
       </motion.div>
     </AnimatePresence>
   )
