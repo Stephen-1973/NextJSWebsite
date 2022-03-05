3e53d5e88b19ad497a80a9257bf16ba7188cf5c9
Stephen-1973 <ststephen426@gmail.com>
2022-03-05 15:54:03 +0530

    RelIcon

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,9 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head/>
+        <Head>
+          <link rel="icon" href="/icon.jpeg" />
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
